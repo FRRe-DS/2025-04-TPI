dca3491562893c172bf4be79cd400a717d754526
EmaZaracho <emazaracho7@gmail.com>
2025-11-29 11:27:38 -0300

    Limpieza de archivos basura

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem Activa el entorno virtual
-call entorno\Scripts\activate
-
-rem Ejecuta el servidor de Django
-python manage.py runserver
-
-rem Desactiva el entorno virtual cuando termines (opcional)
-deactivate
